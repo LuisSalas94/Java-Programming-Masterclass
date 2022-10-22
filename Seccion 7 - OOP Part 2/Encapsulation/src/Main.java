d80857ee58d5c293f95dcfa6ed97b27aaa5d3990
Fernando Salas <luisfernandosalasg@gmail.com>
2022-10-21 20:05:40 -0500

    Encapsulation

---
+++ Seccion 7 - OOP Part 2/Encapsulation/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        EnhancedPlayer player = new EnhancedPlayer("Fernando", 200, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+   }
+}
